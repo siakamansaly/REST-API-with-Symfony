fe6e4a50f216e1acc4f98fc75e257008b12db92a
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-04-25 00:54:16 +0200

    make product management

---
+++ src/DataPersister/ProductDataPersister.php
@@ -0,0 +1,46 @@
+<?php 
+namespace App\DataPersister;
+
+use DateTime;
+use App\Entity\Product;
+use Doctrine\ORM\EntityManagerInterface;
+use ApiPlatform\Core\DataPersister\DataPersisterInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+final class ProductDataPersister implements DataPersisterInterface
+{
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    public function supports($data, array $context = []): bool
+    {
+        return $data instanceof Product;
+    }
+
+    public function persist($data, array $context = [])
+    {
+        
+        // Set values
+        $date = new \DateTime();
+        $data->setCreatedAt($date);
+        $data->setReference(uniqid("REF_"));
+
+        // Doctrine Persister Customer
+        $this->entityManager->persist($data);
+        $this->entityManager->flush();
+    }
+
+    public function remove($data, array $context = [])
+    {
+        // Doctrine Remove Customer
+        $this->entityManager->remove($data);
+        $this->entityManager->flush();
+    }
+
+
+}
