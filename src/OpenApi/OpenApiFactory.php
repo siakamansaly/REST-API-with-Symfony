61e61435134c55498bb36ce8deea4440dc9d7fce
Siaka MANSALY <siaka.mansaly@gmail.com>
2022-05-06 20:37:11 +0200

    add Bearer JWT and disable endpoint

--- src/OpenApi/OpenApiFactory.php
+++ src/OpenApi/OpenApiFactory.php
@@ -1,12 +1,19 @@
 <?php
 namespace App\OpenApi;
 
-use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
+use ArrayObject;
+use ApiPlatform\Core\OpenApi\Model;
 use ApiPlatform\Core\OpenApi\OpenApi;
+use ApiPlatform\Core\OpenApi\Model\PathItem;
+use ApiPlatform\Core\OpenApi\Model\Operation;
+use ApiPlatform\Core\OpenApi\Model\Parameter;
+use Symfony\Component\HttpFoundation\Response;
+use ApiPlatform\Core\OpenApi\Model\RequestBody;
+use ApiPlatform\Core\OpenApi\Factory\OpenApiFactoryInterface;
 
 class OpenApiFactory implements OpenApiFactoryInterface
 {
-    CONST SUMMARY="Hidden";
+    const SUMMARY="Hidden";
     private $decorated;
 
     public function __construct(OpenApiFactoryInterface $decorated)
@@ -17,8 +24,9 @@ class OpenApiFactory implements OpenApiFactoryInterface
     public function __invoke(array $context = []): OpenApi
     {
         $openApi = $this->decorated->__invoke($context);
-        
         $openApi = $this->disableGetPath($openApi);
+        //$openApi = $this->addLogin($openApi);
+        $openApi = $this->addBearer($openApi);
         
         return $openApi;
     }
@@ -27,12 +35,92 @@ class OpenApiFactory implements OpenApiFactoryInterface
     {
         // ** @var PathItem $path */
         foreach ($openApi->getPaths()->getPaths() as $key => $path) {
-            if($path->getGet() && $path->getGet()->getSummary() === self::SUMMARY) {
+            if ($path->getGet() && $path->getGet()->getSummary() === self::SUMMARY) {
                 $openApi->getPaths()->addPath($key, $path->withGet(null));
             }
         }
         return $openApi;
     }
 
+    public function addLogin(OpenApi $openApi): OpenApi
+    {
+        $schemas = $openApi->getComponents()->getSchemas();
 
+        $schemas['Token'] = new \ArrayObject([
+            'type' => 'object',
+            'properties' => [
+                'token' => [
+                    'type' => 'string',
+                    'readOnly' => true,
+                ],
+            ],
+        ]);
+        $schemas['Credentials'] = new \ArrayObject([
+            'type' => 'object',
+            'properties' => [
+                'email' => [
+                    'type' => 'string',
+                    'example' => 'johndoe@example.com',
+                ],
+                'password' => [
+                    'type' => 'string',
+                    'example' => 'apassword',
+                ],
+            ],
+        ]);
+
+        $pathItem = new Model\PathItem(
+            'JWTToken',
+            'Get a JWT Token',
+            'Get a JWT Token',
+            new Model\Operation(
+                'postCredentialsItem',
+                ['Token'],
+                [
+                    '200' => [
+                        'description' => 'Get JWT token',
+                        'content' => [
+                            'application/json' => [
+                                'schema' => [
+                                    '$ref' => '#/components/schemas/Token',
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+                'Get JWT token to login.',
+                new Model\RequestBody(
+                    'Generate new JWT Token',
+                    new \ArrayObject([
+                        'application/json' => [
+                            'schema' => [
+                                '$ref' => '#/components/schemas/Credentials',
+                            ],
+                        ],
+                    ]),
+                ),
+            ),
+        );
+        $openApi->getPaths()->addPath('/authentication_token', $pathItem);
+
+        return $openApi;
+
+    }
+
+
+
+
+    public function addBearer(OpenApi $openApi): OpenApi
+    {
+        $schemas = $openApi->getComponents()->getSecuritySchemes();
+        $schemas['bearerAuth'] = [
+            'type' => 'http',
+            'scheme' => 'bearer',
+            'bearerFormat' => 'JWT',
+        ];
+        $openApi = $openApi->withSecurity([
+            'bearerAuth' => [],
+        ]);
+        return $openApi;
+    }
 }
